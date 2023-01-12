71f6d54c9abaa1463676f07614bc8a84ff48cb8c
Alonso <alonso.carvajal@usach.cl>
2023-01-12 14:24:48 -0300

    Create sql.r

---
+++ sql.r
@@ -0,0 +1,23 @@
+# libreria para la coneción a la base de datos
+#install.packages(DatabaseConnector)
+library(DatabaseConnector)
+
+# Conectarse a la base de datos
+connection_details = createConnectionDetails(
+  dbms = "postgresql",
+  server = "SERVIDOR/BASE DE DATOS",
+  user = "USUARIO",
+  password = "CONTRASENA",
+  pathToDriver = "RUTA AL DRIVER PSQL")
+
+# Para leer la consulta desde un archivo .sql
+#sql = read_file("archivo.sql")
+#sql = render(sql)
+
+# Para añadir directamente la consulta SQL
+sql = "SELECT * FROM person"
+
+# Conectar a la base de datos y ejecutar la consulta
+conn = connect(connectionDetails = connection_details)
+data = dbGetQuery(conn, sql)
+dbDisconnect(conn)
